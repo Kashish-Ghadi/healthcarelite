c656066448eb1df55e8e20569d024e0e3101aca8
Kashish-Ghadi <kashishghadi1614@gmail.com>
2026-03-26 13:19:52 +0530

    css

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD9RhJe2sPFqIj2BWAISZXrmGwIUJnb_i0",
+  authDomain: "healthmonitoring-e9876.firebaseapp.com",
+  databaseURL: "https://healthmonitoring-e9876-default-rtdb.firebaseio.com",
+  projectId: "healthmonitoring-e9876",
+  storageBucket: "healthmonitoring-e9876.firebasestorage.app",
+  messagingSenderId: "662687659674",
+  appId: "1:662687659674:web:e489d32d61f0b61eb09123"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.database();
